1de754d1af677313c1a952a3b0d7e707839db6cd
Dhananjaya Karandana <info.idealsolution@gmail.com>
2020-04-26 16:44:40 +0530

    WebPack Initilization

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+
+const webpack = require('webpack');
+const OUT_DIR_ABS = path.resolve('./dist');
+
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
+module.exports = {
+  name: 'js-files',
+  watch: true,
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: OUT_DIR_ABS,
+    filename: './js/[name].bundle.js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      /* $: 'jquery',
+      jQuery: 'jquery',
+      Handlebars: 'handlebars.runtime' */
+    }),
+    new BrowserSyncPlugin({
+      // browse to http://localhost:3000/ during development,
+      // ./public directory is being served
+      host: 'localhost',
+      port: 3000,
+      server: { 
+        baseDir: ['public'] 
+      },
+      serveStatic: [{
+        route: '/dist',
+        dir: 'dist'
+      }]
+    })
+  ]
+};
