f31c6d281aa6e08ab053f461784e865531a4a1fb
Dhananjaya Karandana <info.idealsolution@gmail.com>
2020-05-02 13:17:19 +0530

    template strings added

---
+++ src/modules/template-strings.js
@@ -0,0 +1,14 @@
+console.info('template-strings.js');
+
+// Basic literal string creation
+console.log( `In JavaScript '\n' is a line-feed.`);
+
+// Multiline strings
+console.log( `In JavaScript this is
+not legal.`
+);
+
+// String interpolation
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
+
